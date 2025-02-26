cb7fe476d82408ca6a75db56020eee43d96c93e9
Dn_Sh <shipilovdanil0@gmail.com>
2025-02-26 22:59:03 +0500

    quote api, styles

--- src/hooks/useQuoteFetch.js
+++ src/hooks/useQuoteFetch.js
@@ -1,27 +1,28 @@
 import { useEffect } from "react";
+const alloriginsWin = "https://api.allorigins.win/get?url=";
+const url = "https://favqs.com/api/qotd";
+// 2 вариант https://strangerthingsquotes.shadowdev.xyz/
 
 function useQuoteFetch(callSetQuote) {
     useEffect(() => {
         async function requestQuote() {
             const savedQuote = JSON.parse(localStorage.getItem('quote'));
             const today = new Date().toISOString().split("T")[0]; // дата ГГГГ-ММ-ДД
-    
+
             if (savedQuote?.quote?.text && savedQuote?.date === today) {
                 callSetQuote({text: savedQuote.quote.text, author: savedQuote.quote.author});
                 return;
             }
     
             try {
-                const response = await fetch(
-                    "https://api.allorigins.win/get?url=" + encodeURIComponent("https://zenquotes.io/api/today")
-                );
+                const response = await fetch(alloriginsWin + encodeURIComponent(url));
                 const data = await response.json();
-                const result = JSON.parse(data.contents);
-    
-                if (result.length > 0) {
+                const dataParsed = JSON.parse(data.contents);   
+                const result = dataParsed.quote;
+                if (result) {
                     const newQuote = {
-                        text: result[0].q,
-                        author: result[0].a || "Unknown",
+                        text: result.body,
+                        author: result.author || "Unknown",
                     }
                     callSetQuote(newQuote);
                     localStorage.setItem('quote', JSON.stringify({quote: newQuote.text, date: today}))
