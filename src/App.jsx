c92681e34c76a4162192b5cfac1e976801335f9f
Dn_Sh <shipilovdanil0@gmail.com>
2025-02-27 22:11:02 +0500

    TermsOfUsePage

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import './css/index.css';
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout.jsx';
 import MainPage from './pages/MainPage.jsx';
-import InfoPage from './pages/infoPage.jsx';
+import InfoPage from './pages/InfoPage.jsx';
 import { SecondLayout } from './layouts/SecondLayout.jsx';
+import TermsOfUsePage from './pages/TermsOfUsePage.jsx';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
       </Route>
       <Route element={<SecondLayout />}>
         <Route path='info' element={<InfoPage />}/>
+        <Route path='terms-of-use' element={<TermsOfUsePage />}/>
       </Route>
     </Routes>
     </>
