658aa12779b2347107d90b8015bd77729c3d0964
Dn_Sh <shipilovdanil0@gmail.com>
2025-03-01 11:35:23 +0500

    formAuth css, SignUp and SignIn pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import MainPage from './pages/MainPage.jsx';
 import InfoPage from './pages/InfoPage.jsx';
 import { SecondLayout } from './layouts/SecondLayout.jsx';
 import TermsOfUsePage from './pages/TermsOfUsePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import SignInPage from './pages/SignInPage.jsx';
 
 function App() {
 
@@ -21,6 +23,8 @@ function App() {
         <Route path='info' element={<InfoPage />}/>
         <Route path='terms-of-use' element={<TermsOfUsePage />}/>
       </Route>
+      <Route path='signup' element={<SignUpPage />} />
+      <Route path='login' element={<SignInPage />} />
     </Routes>
     </>
   )
