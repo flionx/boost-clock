8ffb488a104ac59c38079c52c7e549ca3122d495
Dn_Sh <shipilovdanil0@gmail.com>
2025-03-02 10:34:15 +0500

    correct change theme from firestore

--- src/hooks/useChangeTheme.js
+++ src/hooks/useChangeTheme.js
@@ -1,32 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { setColorTheme } from "../store/slices/settingSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function useChangeTheme() {
 
     const dispatch = useDispatch();
-    const userSettingsTheme = useSelector(state => state.settings.mainSettings.colorTheme);
     // 'light' или 'dark'
-    const [theme, setTheme] = useState(userTheme);
-    function userTheme() {
-        const storage = localStorage.getItem("theme");
-        return storage ? storage : 'light';
-    }
+    const userSettingsTheme = useSelector(state => state.settings.mainSettings.colorTheme);
     
     useEffect(() => {
-        if (theme === 'dark' && userSettingsTheme === 'dark') {
+        if (userSettingsTheme === 'dark') {
             document.documentElement.classList.add("dark-theme");
         } else {
             document.documentElement.classList.remove("dark-theme");
         }
-    }, [theme, userSettingsTheme])
+    }, [userSettingsTheme])
 
     function changeTheme() {
-        const otherTheme = (theme === 'light') ? 'dark' : 'light';
-        setTheme(cur => cur = otherTheme);
-        localStorage.setItem("theme", otherTheme);
+        const otherTheme = (userSettingsTheme === 'light') ? 'dark' : 'light';
         dispatch(setColorTheme(otherTheme))
-        
     }
     return {changeTheme}
 }    
