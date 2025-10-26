00a1768d114abbe77a17ac147a62ad3f21bb64d7
Dn_Sh <shipilovdanil0@gmail.com>
2025-10-26 18:46:01 +0500

    fix(css): dark variables tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center flex-col h-screen">
-      <h1 className="text-4xl">BoostClock</h1>
-      <p className="text-2xl font-nav">Hello</p>
-    </div>
+    <>
+      <main>
+
+      </main>
+    </>
   );
 }
