bbb44f5f57ee0d4d6305981184268077db306edd
Dn_Sh <shipilovdanil0@gmail.com>
2025-11-01 21:39:05 +0500

    feat(header): add a list to MobileMenu

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -4,7 +4,7 @@ import MobileMenu from "./MobileMenu"
 
 const Header = () => {
   return (
-    <header className="bg-secondary py-1.5">
+    <header className="bg-secondary py-1.5 relative">
         <div className="w-[clamp(22rem,58.3vw,70rem)] mx-auto flex items-center justify-between">
             <h1 className="text-4xl">BoostClock</h1>
             <ul className="flex items-center gap-5">
--- src/widgets/header/ui/MobileMenu.tsx
+++ src/widgets/header/ui/MobileMenu.tsx
@@ -1,31 +1,38 @@
 "use client"
 import { useCallback, useState } from "react"
-// import { HEADER_MENU_BUTTONS } from "../constants"
-// import MenuButton from "./MenuButton"
-// import UserButton from "./UserButton"
+import { HEADER_MENU_BUTTONS } from "../constants"
+import MenuButton from "./MenuButton"
+import UserButton from "./UserButton"
 
 const MobileMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = useCallback(() => setIsOpen(c => !c), []);
     
   return (
-    <div className="relative">
+    <div className="min-xl:hidden">
         <button 
             onClick={toggleMenu}
-            className={`flex flex-col justify-between w-6 h-6 min-xl:hidden transition-transform duration-300 ${isOpen ? "-rotate-90" : ""}`}
+            className={`flex flex-col justify-between size-6 transition-transform duration-300 ${isOpen ? "-rotate-90" : ""}`}
         >
             <span className="w-full min-h-[0.15625rem] bg-text rounded-xs"/>
             <span className="w-full min-h-[0.15625rem] bg-text rounded-xs"/>
             <span className="w-full min-h-[0.15625rem] bg-text rounded-xs"/>
         </button>
-        {/* <div className='hidden items-center gap-5'>
-            {HEADER_MENU_BUTTONS.map(m => 
-                <MenuButton key={m.label} icon={m.icon}>
-                    {m.label}
-                </MenuButton>
-            )}
-            <UserButton />
-        </div> */}
+        {isOpen && (
+            <ul className="
+                flex flex-col-reverse items-center justify-around 
+                w-63 h-50 bg-secondary font-nav rounded-sm
+                absolute top-13 left-7/10 -translate-x-[50%]
+                shadow shadow-secondary border-1 border-[#3a3a3a]"
+            >
+                <UserButton />
+                {HEADER_MENU_BUTTONS.map(m => 
+                    <MenuButton key={m.label} icon={m.icon}>
+                        {m.label}
+                    </MenuButton>
+                )}
+            </ul>
+        )}
     </div>
   )
 }
