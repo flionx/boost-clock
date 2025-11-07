7e4291c31dd8bf9695b3a7fb2a3880c1a299887c
Dn_Sh <shipilovdanil0@gmail.com>
2025-11-07 20:01:43 +0500

    feat: add timer player store

---
+++ src/shared/store/timer-player.ts
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+import { useTimerSettingsStore } from "./timer-settings";
+
+export type TimerMode = "work" | "break" | "longBreak";
+
+interface TimerPlayerState {
+    mode: TimerMode,
+    timeLeft: number,
+    isRunning: boolean,
+    toggle: VoidFunction,
+    skip: VoidFunction,
+    reset: VoidFunction,
+    tick: VoidFunction,
+    switchMode: (mode: TimerMode) => void,
+}
+
+export const timerPlayerStore = create<TimerPlayerState>((set, get) => ({
+    mode: "work" as const,
+    timeLeft: useTimerSettingsStore.getState().workDuration * 60,
+    isRunning: false,
+    toggle: () => set(c => ({isRunning: !c.isRunning})),
+    skip: () => {
+        const nextMode = get().mode === "work" ? "break" : "work";
+        get().switchMode(nextMode);
+    },
+    reset: () => {
+        const {mode} = get();
+        const settings = useTimerSettingsStore.getState();
+        const minutes = mode === "work" ? settings.workDuration : 
+            mode === "break" ? settings.breakDuration : settings.longBreakDuration;
+        set({timeLeft: minutes * 60, isRunning: false})
+    },
+    tick: () => {
+        const {timeLeft} = get();
+        if (timeLeft > 0) set({timeLeft: timeLeft - 1})
+        get().skip();
+    },
+    switchMode: (mode) => {
+        const settings = useTimerSettingsStore.getState();
+        const minutes = mode === "work" ? settings.workDuration : 
+            mode === "break" ? settings.breakDuration : settings.longBreakDuration;
+        set({mode, timeLeft: minutes * 60, isRunning: false})
+    }
+}))
--- src/widgets/timer/ui/TimerPlayer.tsx
+++ src/widgets/timer/ui/TimerPlayer.tsx
@@ -5,7 +5,7 @@ const TimerPlayer = () => {
     <div className="mx-auto bg-accent pt-2.5 px-4 sm:px-27.5 pb-10 max-w-112.5 rounded-lg mb-2.5">
       <div className="flex items-center justify-center gap-5 mb-8.5">
         <ToggleTimerButton type="Work" />
-        <span className="bg-white h-8.5 w-[3.2]" />
+        <span className="bg-white h-8.5 min-w-[3.2]" />
         <ToggleTimerButton type="Break"/>
       </div>
       <h2 className="text-[5.3125rem] text-center mb-12.5 text-white" translate="no">5:00</h2>
