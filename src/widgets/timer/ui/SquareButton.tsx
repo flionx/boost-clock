25697d557ecc826af6bb1c1beb96878d45532d4f
Dn_Sh <shipilovdanil0@gmail.com>
2025-11-07 19:28:04 +0500

    fix: add hover and active effects for SquareButton

--- src/widgets/timer/ui/SquareButton.tsx
+++ src/widgets/timer/ui/SquareButton.tsx
@@ -11,7 +11,10 @@ const SquareButton: React.FC<SquareButtonProps> = ({
   return (
     <button 
         // onClick={onClick}
-        className="size-7.5 bg-white rounded-xl text-black font-bold hover:bg-gray-200"
+        className="
+            size-7.5 bg-white rounded-xl text-black font-bold duration-200 transition-all 
+            hover:bg-white/90 hover:scale-109 active:bg-white active:scale-none
+        "
     >
         {children}
     </button>
