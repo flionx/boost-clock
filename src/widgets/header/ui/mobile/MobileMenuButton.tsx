9ddb6847585871c19e31a264df9d1e9008fddf46
Dn_Sh <shipilovdanil0@gmail.com>
2026-05-04 13:38:47 +0500

    fix: close mobile menu when click outside

--- src/widgets/header/ui/mobile/MobileMenuButton.tsx
+++ src/widgets/header/ui/mobile/MobileMenuButton.tsx
@@ -1,16 +1,35 @@
 "use client"
 import { useAchievementsStore } from '@/features/achievements/store/achievements';
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import UnseenNotify from '../UnseenNotify';
 import MobileMenu from './MobileMenu';
 
 const MobileMenuButton = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = useCallback(() => setIsOpen(c => !c), []);
-  const newUnseenAchievs = useAchievementsStore(state => state.newUnseenAchievs)
+  const closeMenu = useCallback(() => setIsOpen(false), []);
+  const newUnseenAchievs = useAchievementsStore(state => state.newUnseenAchievs);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        closeMenu();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [isOpen, closeMenu]);
 
   return (
-    <div className="xl:hidden relative">
+    <div className="xl:hidden relative" ref={menuRef}>
       <button
         onClick={toggleMenu}
         className={`flex flex-col justify-between size-6 transition-transform duration-300 ${isOpen ? "-rotate-90" : ""}`}
